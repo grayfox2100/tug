c537d39889f9382a55f9793a8740491d3754e260
grayfox2100 <email321@mail.ru>
2021-01-18 15:03:23 +0900

    Borders spawn in level gen

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -7,14 +7,34 @@ public class LevelGen : MonoBehaviour
     public int levelSizeX = 32;
     public int LevelSizeY = 8;
     public GameObject LavaPrefab;
+    public GameObject BlockPrefab;
     
     // Start is called before the first frame update
     void Start()
     {
-        // Make lava{
-        for (int x = 0; x < (levelSizeX + 1); x++)
+        // Make lava {
+        int lavaCount = 0;
+        for (int x = 1; x < (levelSizeX + 1); x++)
         {
-            Instantiate(LavaPrefab.gameObject, new Vector3(x,1),Quaternion.identity);
+            Instantiate(LavaPrefab, new Vector3(x,1),Quaternion.identity);
+        }
+        // }
+        
+        // Make borders {
+        for (int y = 0; y < (LevelSizeY + 2); y++)
+        {
+            if (y == 0 || y == (LevelSizeY + 1))
+            {
+                for (int x = 0; x < (levelSizeX + 2); x++)
+                {
+                    Instantiate(BlockPrefab, new Vector3(x,y),Quaternion.identity); // Vertical borders
+                }
+            }
+            else
+            {
+                Instantiate(BlockPrefab, new Vector3(0, y),Quaternion.identity); // Lower horizontal border
+                Instantiate(BlockPrefab, new Vector3((levelSizeX + 1), y),Quaternion.identity); // Upper horizontal border
+            }
         }
         // }
     }
