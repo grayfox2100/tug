9522655260ed2a1663f90c331e94f63666ec607b
grayfox2100 <email321@mail.ru>
2021-01-18 10:52:11 +0900

    player, enemy, blocks, lava, finish

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 500.0f;
+    public float weightMin = 1.0f;
+    public float weightMax = 5.0f;
+    
+    private Rigidbody2D _body;
+    private CircleCollider2D _enemyCollider;
+    private int enemyWeight;
+    private float enemySize;
+    private int moveDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _enemyCollider = GetComponent<CircleCollider2D>();
+
+        // Generation enemy stats{
+        System.Random rnd = new System.Random();
+        int playerTier = rnd.Next(1,6); // Enemy size from 0.5f to 1.0f
+        switch (playerTier)
+        {
+            case 1: 
+                this.enemySize = 0.5f;
+                break;            
+            case 2: 
+                this.enemySize = 0.6f;
+                break;            
+            case 3: 
+                this.enemySize = 0.7f;
+                break;            
+            case 4: 
+                this.enemySize = 0.8f;
+                break;            
+            case 5: 
+                this.enemySize = 0.9f;
+                break;
+            default:
+                this.enemySize = 1.0f;
+                break;
+        }
+        this.enemyWeight = (int)Math.Round((((weightMax - weightMin) / 6) * playerTier) + 1);
+        // }
+        
+        _body.mass = this.enemyWeight;
+        transform.localScale = new Vector3(enemySize,enemySize);
+        moveDirection = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Enemy moving{
+        float deltaX = moveDirection * speed * Time.deltaTime; // Calculate power of velocity
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y); // Make a new v2 for player velocity along X axis
+        _body.velocity = movement; // Assign new velocity value
+        // }
+        
+        int layerMask = LayerMask.GetMask("Blocks");
+        // Wall check{
+        Vector2 wallCheckDirection = moveDirection > 0 ? Vector2.right : Vector2.left;
+        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, wallCheckDirection, 0.75f, layerMask);
+        
+        Debug.DrawRay(transform.position, wallCheckDirection, Color.yellow);
+        
+        if (hitWall.collider != null)
+        {
+            Debug.Log("wall");
+            this.moveDirection *= -1;
+        }
+        // }
+        
+        // Floor check{
+        Vector2 floorCheckDirection = new Vector2(moveDirection > 0 ? 1.0f : -1.0f, -0.5f);
+        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, floorCheckDirection, 1.0f, layerMask);
+        
+        Debug.DrawRay(transform.position, floorCheckDirection, Color.red);
+        
+        if (hitFloor.collider == null)
+        {
+            Debug.Log("floor");
+            this.moveDirection *= -1;
+        }
+        // }
+
+    }
+    
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 500.0f;
+    public float jumpForce = 12.0f;
+    public float weightMin = 1.0f;
+    public float weightMax = 5.0f;
+    public float livesMin = 1.0f;
+    public float livesMax = 3.0f;
+    
+    private Rigidbody2D _body;
+    private CircleCollider2D _playerCollider;
+    private int playerWeight;
+    private int playerLives;
+    private float playerSize;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _playerCollider = GetComponent<CircleCollider2D>();
+
+       // Generation player stats{
+        System.Random rnd = new System.Random();
+        int playerTier = rnd.Next(1,6); // Player size from 0.5f to 1.0f
+        switch (playerTier)
+        {
+            case 1: 
+                this.playerSize = 0.5f;
+                break;            
+            case 2: 
+                this.playerSize = 0.6f;
+                break;            
+            case 3: 
+                this.playerSize = 0.7f;
+                break;            
+            case 4: 
+                this.playerSize = 0.8f;
+                break;            
+            case 5: 
+                this.playerSize = 0.9f;
+                break;
+            default:
+                this.playerSize = 1.0f;
+                break;
+        }
+        this.playerWeight = (int)Math.Round((((weightMax - weightMin) / 6) * playerTier) + 1);
+        this.playerLives = (int)Math.Round((((livesMax - livesMin) / 6) * playerTier) + 1);
+        // }
+        
+        _body.mass = this.playerWeight;
+        transform.localScale = new Vector3(playerSize,playerSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player moving{
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // Calculate power of velocity
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y); // Make a new v2 for player velocity along X axis
+        _body.velocity = movement; // Assign new velocity value
+        // }
+
+        // Player grounded checking{
+        Vector3 max = _playerCollider.bounds.max; 
+        Vector3 min = _playerCollider.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = false || hit != null;
+        
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        // }
+        
+        // Player lives check
+        if (playerLives < 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("You Died!");
+        }
+        // }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            this.playerLives--;
+            Debug.Log("Player recive damage! Lives left: " + this.playerLives);
+        } else if (collision.gameObject.CompareTag("Death"))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("You Died!");
+        } else if (collision.gameObject.CompareTag("Finish"))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("You Died! But win.");
+        }
+    }
+}
