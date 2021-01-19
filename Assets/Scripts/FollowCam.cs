6105f2804c37ee647173f4e8531212b8f1fa1a29
grayfox2100 <email321@mail.ru>
2021-01-19 17:40:38 +0900

    Watchin cam

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	//public Transform target;
+	public float smoothTime = 0.2f;
+	private Vector3 _velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//Debug.Log();
+		Vector3 camTarget = GameObject.FindWithTag("Player").transform.position;
+		Vector3 targetPosition;
+		
+		if (camTarget != null)
+		{
+			targetPosition = new Vector3(camTarget.x, camTarget.y, transform.position.z);
+			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+		}
+		
+	}
+}
